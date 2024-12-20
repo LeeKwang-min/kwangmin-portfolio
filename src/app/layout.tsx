7f97bdbaa65826b757d917242bfa41864eeec70f
LeeKwang-min <dltkdtn56@naver.com>
2024-12-20 22:06:15 +0900

    [수정] 당장 사용하지 않는 pdfHeader 컴포넌트 임포트문 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import PdfHeader from "@/components/common/PdfHeader";
+// import PdfHeader from "@/components/common/PdfHeader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
