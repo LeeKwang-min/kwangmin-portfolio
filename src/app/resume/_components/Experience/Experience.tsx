acf20ef268fd809ee911e639ee80e8645da94f49
LeeKwang-min <dltkdtn56@naver.com>
2025-03-06 23:10:08 +0900

    [수정] 자기소개, 회사 경력 부분 수정

--- src/app/resume/_components/Experience/Experience.tsx
+++ src/app/resume/_components/Experience/Experience.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 const experienceList = [
@@ -5,55 +6,99 @@ const experienceList = [
     company: "휴레이포지티브",
     position: "프론트엔드 개발자",
     period: "2022.02 - 현재",
-    description: "사용자 평가를 활용한 UX 중심적 백오피스 웹 서비스 개발",
+    description: `"UX 중심의 백오피스 및 서비스 개발로 비즈니스 가치 향상"`,
     tasks: [
-      "백오피스 웹 애플리케이션 기획, 디자인, 개발 및 유지보수",
-      "UX 개선 및 성능 최적화를 통한 사용자 경험 개선",
-      "A/B 테스트 활용한 효율성 향상 및 사용자 평가 활용",
-      "서비스 통합 관리 백오피스 프로젝트 주도 (2024.01 - 진행중)",
-      "음식 인식 애플리케이션 프로젝트 주도 (2024.06 - 2024.09)",
-      "헬스케어 서비스 및 백오피스 웹 애플리케이션 개발 (2022.02 - 2024.01)",
+      {
+        title: "백오피스 웹 애플리케이션 개발 및 유지보수, 프로젝트 주도",
+        content: [
+          "개발부터 유지보수까지 주도하며 서비스 운영의 안정성과 확장성 확보",
+        ],
+      },
+      {
+        title: "사용자 피드백 기반 UX 개선 및 성능 최적화",
+        content: [
+          "사용자 요청에 대한 즉각적인 반응성 강화",
+          "❯ API 요청 중 로딩 UI 제공 및 피드백 시스템 구축(토스트 메시지, 오류 경고)",
+          "사용자 입력 편의성 향상, 모바일 사용성 개선",
+          "❯ 자동 포커스 이동, 페이지 스크롤 유지, TOP 버튼 및 UI 배치 최적화",
+          "❯ 키보드 사용자를 고려한 Tabindex 설정으로 접근성 강화",
+        ],
+      },
+      {
+        title: "성능 최적화 및 로딩 속도 개선",
+        content: [
+          "서버 호출 및 불필요한 리렌더링 최소화",
+          "❯ Debounce가 적용된 테이블 필터 제공",
+          "초기 페이지 로딩 속도 단축, 컴포넌트 성능 최적화",
+          "❯ 이미지, 컴포넌트 Lazy Loading 적용, 코드 스플리팅을 통한 Next.js 하이드레이션 활용",
+        ],
+      },
+      {
+        title: "PWA 적용 및 반응형 디자인 구현으로 모바일 최적화",
+        content: [
+          "서비스 접근성 강화, 스크롤 네비게이션 최적화",
+          "❯ 모바일 홈 화면 서비스 추가, TOP 버튼",
+          "터치 인터랙션 최적화 및 사용성 극대화",
+          "❯ 버튼 애니메이션 적용, UX 배치 개선",
+        ],
+      },
     ],
     projects: [
       {
-        title: "사내 서비스 통합 관리 백오피스 프로젝트",
+        title: "검진대행 사용자 서비스 (2024.12 ~ 현재)",
+        tasks: ["작성중"],
+        stacks: [
+          "React, TypeScript, Next.js, Tanstack-Table, React-Hook-Form, Tailwind CSS, Shadcn UI",
+        ],
+      },
+      {
+        title: "라이프로그 통합 관리 백오피스 (2024.01 - 2024.12)",
         tasks: [
-          "클라이언트 기획부터 디자인, 개발까지 주도하여 리딩",
-          "Jira를 통한 유저 스토리 정의 및 프로젝트 관리",
-          "Debounce를 활용한 UI를 적용하여 성능 최적화 및 사용자 경험 개선",
-          "수작업으로 진행되던 프로세스를 로직/자동화하여 사용자 편의성 향상",
-          "실제 사용자의 피드백을 반영한 UX 개선 및 효율성 향상",
-          "A/B 테스트를 통해 사용자 효율성과 편의성 향상",
-          "실제 사용자와의 꾸준한 소통을 통한 요구사항의 이해와 개발 진행",
+          "성능 최적화 및 초기 로딩 속도 개선",
+          "❯ Debounce 적용한 테이블 필터를 활용하여 서버 호출 횟수 및 화면 리렌더링 최소화",
+          "❯ Lazy Loading 적용 및 코드 스플리팅",
+          "사용자 경험 우선 UX 개선 및 성능 최적화",
+          "❯ 사용자 피드백을 반영한 UX 개선, 주요 기능 재설계를 통해 사용 편의성 증가",
+          "❯ API 요청 중 로딩 UI 제공 및 토스트 메시지, 오류 경고 시스템 구축",
+          "❯ 자동 포커스, 스크롤 Tabindex 설정으로 접근성 강화",
+          "사용자 인터랙션 분석 후 최적 레이아웃 적용 및 기능 최적화",
+          "클라이언트 개발 리딩, 프로세스 자동화 및 서비스 운영의 안정화와 확장",
+          "Jira 기반 요구사항 정의 및 프로젝트 관리, 효율적인 협업 환경 구축",
         ],
         stacks: [
           "React, TypeScript, Next.js, Tanstack-Table, React-Hook-Form, Tailwind CSS, Shadcn UI",
         ],
       },
       {
-        title: "음식 인식 애플리케이션 프로젝트",
+        title: "음식 인식 애플리케이션 (2024.06 - 2024.09)",
         tasks: [
-          "PWA 적용을 통한 모바일 최적화 진행",
-          "반응형 디자인을 적용하여 웹과 모바일 환경에서 자연스러운 UI 제공",
-          "Next-Auth와 Middleware를 활용한 클라이언트 단독 인증 시스템 구현",
-          "도메인 이해를 바탕으로 추가 기능 기획 및 개발",
+          "PWA 적용 및 반응형 디자인 구현으로 모바일 최적화",
+          "❯ 간편 접속 제공, URL 입력이나 검색 없이 홈 화면에서 바로 접근 가능",
+          "❯ 모바일 UX/UI 최적화, 모바일 맞춤 배치 및 버튼 추가로 사용성 강화",
+          "❯ 버튼 애니메이션 적용 및 터치 인터랙션 최적화",
+          "보안성과 접근성 개선",
+          "❯ Next-Auth와 Middleware 기반 인증 시스템 구축으로 보안 강화",
+          "❯ 브라우저 저장소를 활용한 인식된 음식별 적합 영양 정보 추천/제공",
         ],
         stacks: [
           "React, TypeScript, Next.js, Tailwind CSS, Shadcn UI, Next-Auth, Next-PWA",
         ],
       },
       {
-        title: "헬스케어 서비스 및 백오피스 웹 애플리케이션 개발",
+        title: "헬스케어 서비스 및 백오피스 (2022.02 - 2024.01)",
         tasks: [
-          "Git을 활용한 코드 리뷰 및 커밋 컨벤션 정의",
-          "Confluence를 통한 문서 관리 및 공유",
-          "Sentry를 통한 에러 추적 및 모니터링",
-          "웹뷰 프로젝트 개발 진행",
-          "디자인 시스템 개발 진행",
-          "공통 차트 컴포넌트 개발",
-          "상태 관리 라이브러리를 통한 데이터 관리 및 렌더링 최적화",
-          "i18n을 통한 다국어 지원",
-          "RESTful API 활용",
+          "사용자 경험, 편의성 향상",
+          "❯ API 요청 중 로딩 UI 제공",
+          "❯ 사용자 입력 검증, 즉각적인 피드백 시스템 구축",
+          "❯ 페이지 스크롤 유지 및 자동 포커스 이동",
+          "개발 생산성 향상, 개발 문화 정의",
+          "❯ Git을 활용한 코드 리뷰 및 커밋 컨벤션 정의로 개발 생산성 향상",
+          "❯ Confluence를 통한 문서 관리 및 지식 공유, 프로젝트 유지보수 용이성 확보",
+          "서비스 안정성 및 성능 개선",
+          "❯ Sentry 기반 에러 모니터링 및 대응 프로세스 구축",
+          "❯ 공통 차트 컴포넌트 개발, 데이터 시각화 및 사용자 인터랙션 최적화",
+          "사용자 피드백 기반 UX 개선 및 글로벌 접근성 강화",
+          "❯ 다국어 지원(i18n) 적용, 글로벌 사용자 대상 접근성 개선",
         ],
         stacks: [
           "React, TypeScript, Next.js, Recoil, Chart.js, Axios, Tanstack-Query, React-Hook-Form, Tailwind CSS",
@@ -76,20 +121,40 @@ function Experience() {
               <h3 className="text-2xl font-semibold text-gray-700">
                 {item.company}
               </h3>
-              <p className="text-gray-600">{item.period}</p>
+              <div className="flex items-center gap-8">
+                <h4 className="font-semibold text-gray-900">{item.position}</h4>
+                <p className="text-gray-600">{item.period}</p>
+              </div>
             </div>
             <div className="w-full md:w-2/3 space-y-4">
               <div className="border-l-4 border-gray-200 pl-4">
-                <h4 className="text-xl font-semibold text-gray-900">
-                  {item.position}
-                </h4>
-                <p className="mb-2">{item.description}</p>
-                <h5 className="text-lg font-semibold text-gray-800 mb-2">
+                <p className="text-gray-900 font-bold text-xl py-8">
+                  {item.description}
+                </p>
+                <h5 className="text-xl font-bold text-gray-800 mb-2">
                   주요 업무
                 </h5>
-                <ul className="mb-2 flex flex-col gap-2 pb-4">
+                <ul className="mb-2 flex flex-col gap-2 pb-4 border-b-2">
                   {item.tasks.map((task) => (
-                    <li key={task}>{`- ${task}`}</li>
+                    <li key={task.title}>
+                      <h6 className="text-lg font-semibold text-gray-800 mb-1">
+                        {task.title}
+                      </h6>
+                      <div className="text-base whitespace-pre-wrap">
+                        {task.content.map((content) => {
+                          const isSubTask = content.startsWith("❯");
+
+                          return (
+                            <p key={content} className="pl-4 leading-7 flex">
+                              {!isSubTask && <span className="mr-2">•</span>}
+                              <span className={cn(isSubTask && "pl-4")}>
+                                {content}
+                              </span>
+                            </p>
+                          );
+                        })}
+                      </div>
+                    </li>
                   ))}
                 </ul>
                 {item.projects.map((project) => (
@@ -97,10 +162,26 @@ function Experience() {
                     <h5 className="text-lg font-semibold text-gray-600 mb-2">
                       {project.title}
                     </h5>
-                    <ul className="list-disc list-inside mb-2 leading-8">
-                      {project.tasks.map((task) => (
-                        <li key={task}>{task}</li>
-                      ))}
+                    <ul className="mb-2 leading-8">
+                      {project.tasks.map((task) => {
+                        const isSubTask = task.startsWith("❯");
+
+                        return (
+                          <li
+                            key={task}
+                            className={cn(
+                              "list-disc list-inside",
+                              isSubTask && "list-none",
+                              !isSubTask &&
+                                "text-xl text-gray-800 font-semibold leading-8"
+                            )}
+                          >
+                            <span className={cn(isSubTask && "pl-4")}>
+                              {task}
+                            </span>
+                          </li>
+                        );
+                      })}
                     </ul>
                     <h5 className="text-lg font-semibold text-gray-600 mb-2">
                       기술 스택
--- src/app/resume/_components/Introduction/Introduction.tsx
+++ src/app/resume/_components/Introduction/Introduction.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 
-const introduction = `"사용자 친화적 서비스를 만드는것을 최우선으로 생각하는" 프론트엔드 개발자입니다. 
-팀원과의 소통을 가장 중요하게 생각하며, 협업을 통해 더 나은 서비스를 만들어가는 것을 목표로 하고 있습니다.
-동료 평가를 통해 '팀 리더로 적합한', '문제 해결 능력이 뛰어난', '팀의 신뢰를 가장 많이 받은', '가장 책임감 있는' 동료로 평가받았습니다.
-도메인을 이해하고 이해를 바탕으로 문제를 해결하는 것을 중요하게 생각하며, 그 과정에서 성취감을 느끼고 있습니다.
-좋은 개발자를 넘어 동료가 되기 위해 노력합니다.
-`;
-
 function Introduction() {
   return (
     <section className="mb-12">
       <h2 className="text-3xl font-semibold mb-4 border-b-2 pb-2">ABOUT ME</h2>
-      <p className="text-lg leading-8">{introduction}</p>
+      {/* <p className="text-lg leading-8">{introduction}</p> */}
+      <ul className="list-disc list-inside text-base leading-8">
+        <li>
+          <b>사용자 경험</b>을 최우선으로 생각하는 웹 프론트엔드 개발자입니다.
+        </li>
+        <li>
+          사용자에게 좋은 경험을 제공하는 일에 보람을 느끼며, 다양한 도전에 열려
+          있습니다.
+        </li>
+        <li>
+          적극적인 <b>소통과 협업</b>을 통해, <b>함께 일하고 싶은 동료</b>가
+          되기 위해 노력합니다.
+        </li>
+        <li>
+          새로운 기술에 흥미를 가지지만, 문제의 본질을 깊이 파악하는 것을
+          중요하게 여깁니다.
+        </li>
+        <li>
+          동료 평가에서 <b>{`"팀 리더로 적합한"`}</b>,{" "}
+          <b>{`"문제 해결 능력이 뛰어난"`}</b>,{" "}
+          <b>{`"팀의 신뢰를 가장 많이 받은"`}</b>, <b>{`"가장 책임감 있는"`}</b>{" "}
+          동료로 평가받았습니다.
+        </li>
+      </ul>
     </section>
   );
 }
